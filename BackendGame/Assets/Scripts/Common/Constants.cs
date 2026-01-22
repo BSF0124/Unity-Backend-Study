f8046580063394711dcccdecd04706f944c97494
Yunjae Na <150145660+BSF0124@users.noreply.github.com>
2026-01-23 06:05:05 +0900

    "랭킹" 기능을 이용한 유저 랭킹 관리 (랭킹 출력)
    - Lobby 씬 TabBar 제작
    - Rank 탭 제작
    - 랭킹 데이터 불러오기

--- BackendGame/Assets/Scripts/Common/Constants.cs
+++ BackendGame/Assets/Scripts/Common/Constants.cs
@@ -2,5 +2,6 @@ public static class Constants
 {
     public static readonly string USER_DATA_TABLE = "USER_DATA";
 	public static readonly string DAILY_RANK_UUID = "019be482-47d4-7ed6-8040-c850332db244";
+    public static readonly int MAX_RANK_LIST = 20;
 	
 }
