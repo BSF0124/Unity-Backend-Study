4269211fbf92fef77b04eaa902af165506c074fb
Yunjae Na <150145660+BSF0124@users.noreply.github.com>
2026-01-23 10:46:27 +0900

    "우편" 기능을 이용한 유저 우편 관리 (Console View)
    - 재화 차트 생성 및 등록
    - 우편 종류, 만료일, 삭제 시점
    - 콘솔에서 우편 발송
    - 수령 가능한 우편 리스트 불러오기
    - 우편 하나 수령
    - 우편 전체 수령

--- BackendGame/Assets/Scripts/Common/Constants.cs
+++ BackendGame/Assets/Scripts/Common/Constants.cs
@@ -4,5 +4,5 @@ public static class Constants
 	public static readonly string DAILY_RANK_UUID = "019be482-47d4-7ed6-8040-c850332db244";
     public static readonly int MAX_RANK_LIST = 20;
     public static readonly string LEVEL_CHART = "224848";
-	
+    public static readonly string GOODS_CHART_NAME = "재화차트";
 }
